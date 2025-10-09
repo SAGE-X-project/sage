8cf68dde62ff0eb4f9a91f6ac068f987c5d356ce
0xTopaz <kevin.dev201@gmail.com>
2025-10-10 03:41:56 +0900

    Fix Java SDK test - correct Ethereum address length
    - Fixed Ethereum address in DidTest from 39 to 40 characters (excluding 0x)
    - All 17 Java SDK tests now passing (9 DID tests + 8 Crypto tests)
    - BUILD SUCCESS

--- sdk/java/sage-client/src/test/java/com/sage/client/DidTest.java
+++ sdk/java/sage-client/src/test/java/com/sage/client/DidTest.java
@@ -11,12 +11,12 @@ class DidTest {
 
     @Test
     void testValidEthereumDid() throws SageException.DidException {
-        String didString = "did:sage:ethereum:0x742d35Cc6634C0532925a3b844Bc9e7595f0bEb";
+        String didString = "did:sage:ethereum:0x742d35Cc6634C0532925a3b844Bc9e7595f0bEbC";
         Did did = new Did(didString);
 
         assertEquals(didString, did.toString());
         assertEquals("ethereum", did.getNetwork());
-        assertEquals("0x742d35Cc6634C0532925a3b844Bc9e7595f0bEb", did.getAddress());
+        assertEquals("0x742d35Cc6634C0532925a3b844Bc9e7595f0bEbC", did.getAddress());
     }
 
     @Test
@@ -41,9 +41,9 @@ class DidTest {
 
     @Test
     void testFromParts() throws SageException.DidException {
-        Did did = Did.fromParts("ethereum", "0x742d35Cc6634C0532925a3b844Bc9e7595f0bEb");
+        Did did = Did.fromParts("ethereum", "0x742d35Cc6634C0532925a3b844Bc9e7595f0bEbC");
 
-        assertEquals("did:sage:ethereum:0x742d35Cc6634C0532925a3b844Bc9e7595f0bEb", did.toString());
+        assertEquals("did:sage:ethereum:0x742d35Cc6634C0532925a3b844Bc9e7595f0bEbC", did.toString());
         assertEquals("ethereum", did.getNetwork());
     }
 
@@ -68,7 +68,7 @@ class DidTest {
 
     @Test
     void testIsValid() {
-        assertTrue(Did.isValid("did:sage:ethereum:0x742d35Cc6634C0532925a3b844Bc9e7595f0bEb"));
+        assertTrue(Did.isValid("did:sage:ethereum:0x742d35Cc6634C0532925a3b844Bc9e7595f0bEbC"));
         assertTrue(Did.isValid("did:sage:test:alice"));
         assertFalse(Did.isValid("invalid-did"));
         assertFalse(Did.isValid("did:sage:ethereum:invalid"));
@@ -76,8 +76,8 @@ class DidTest {
 
     @Test
     void testEquality() throws SageException.DidException {
-        Did did1 = new Did("did:sage:ethereum:0x742d35Cc6634C0532925a3b844Bc9e7595f0bEb");
-        Did did2 = new Did("did:sage:ethereum:0x742d35Cc6634C0532925a3b844Bc9e7595f0bEb");
+        Did did1 = new Did("did:sage:ethereum:0x742d35Cc6634C0532925a3b844Bc9e7595f0bEbC");
+        Did did2 = new Did("did:sage:ethereum:0x742d35Cc6634C0532925a3b844Bc9e7595f0bEbC");
         Did did3 = new Did("did:sage:ethereum:0x123456789abcdef0123456789abcdef012345678");
 
         assertEquals(did1, did2);
