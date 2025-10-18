a65bb9a473cfeeda5a2cfc36e042c7d2a60f91db
0xTopaz <kevin.dev201@gmail.com>
2025-10-18 17:35:00 +0900

    fix(build): Add build tag to exclude example.go from tests
    Add //go:build ignore tag to contracts/ethereum/bindings/go/example.go
    to prevent relative import error during CI/CD tests.
    
    The example.go file uses relative imports (./registry) which are not
    supported in Go module mode. This file is meant as documentation/example
    only and should not be built or tested.
    
    Fixes CI error:
    - Error: relative import paths are not supported in module mode
    - Error: local import "./registry" in non-local package

--- contracts/ethereum/bindings/go/example.go
+++ contracts/ethereum/bindings/go/example.go
@@ -16,6 +16,9 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with SAGE. If not, see <https://www.gnu.org/licenses/>.
 
+//go:build ignore
+// +build ignore
+
 package main
 
 import (
